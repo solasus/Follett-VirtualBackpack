64a2c8eacfcc8125ad6a6576c500f97ab4ad9ebf
Kevin Brady <kevin@solasus.com>
2017-07-24 01:18:56 -0400

    ..

--- js/general.js
+++ js/general.js
@@ -12,7 +12,12 @@ function cbLogin() {
 		$.mobile.navigate('#settings');
 	} else { // yes cookie
 		// post to ektron
-		$('#webapp').attr('src', 'login.html')
+		var iframe = document.getElementById("webapp");
+		iframe.onload = function(){
+			iframe.contentWindow.cordova = window.cordova;
+		};
+		iframe.src = 'login.html';
+		//$('#webapp').attr('src', 'login.html')
 	}
 }
 
@@ -56,19 +61,45 @@ $(document).on('pagebeforeshow','#settings' ,function(e,data){
 		e.preventDefault();
 		var target = "https://virtualbackpack.follett.com/default.aspx";
 		var cbname = $.cookie('cbname');
-		if (cbname.substring(0, 1) == "*") { // set action for redirect to staging
+		if (cbname.substring(0, 1) === "*") { // set action for redirect to staging
 			target = "https://virtualbackpackstage.follett.com/default.aspx"; 
 		}
 		$('#webapp').attr('src',target);
 	});
 	
+	var iframe = document.getElementById("webapp2");
+	iframe.onload = function(){
+		iframe.contentWindow.cordova = window.cordova;
+	};
+	iframe.src = 'https://solasus.com/html/clients/da/follett_vbp/link_test.html';
+	
 });
 
-$(document).on('pagebeforeshow','#pass' ,function(e,data){    
+$(document).on('pagebeforeshow','#forgotpass' ,function(e,data){    
 	setHeight();
+	var iframe = document.getElementById("forgotpassword");
+	iframe.onload = function(){
+		iframe.contentWindow.cordova = window.cordova;
+	};
+	iframe.src = 'https://virtualbackpack.follett.com/forgotpassword.aspx';
+});
+
+$(document).on('pagebeforeshow','#changepassword' ,function(e,data){    
+	setHeight();
+	var iframe = document.getElementById("changepassword");
+	iframe.onload = function(){
+		iframe.contentWindow.cordova = window.cordova;
+	};
+	iframe.src = 'https://virtualbackpack.follett.com/changepassword.aspx';
 });
 
 $(document).on('pagebeforeshow','#site' ,function(e,data){    
 	setHeight();
+	var iframe = document.getElementById("efollettstore");
+	iframe.onload = function(){
+		iframe.contentWindow.cordova = window.cordova;
+	};
+	iframe.src = 'https://www.bkstr.com/efollettstore/home/';
 });
+
 
